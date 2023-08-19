fcef4bbab4a67ec98037929dd255c09f9f925f20
Angeles Cahill <angeles.cahill@gmail.com>
2023-08-19 10:04:30 -0400

    subiendo a repo

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,4 +1,6 @@
-const { DataTypes } = require("sequelize");
+const {
+    DataTypes
+} = require("sequelize");
 const sequelize = require("../config/db.config");
 
 // Creación de la tabla user
@@ -12,10 +14,26 @@ const User = sequelize.define(
         firstname: {
             type: DataTypes.STRING,
             allowNull: false,
+            validate: {
+                notNull: {
+                    msg: "Name es requerido",
+                },
+                notEmpty: {
+                    msg: "Debe ingresar un Name",
+                },
+            },
         },
         lastname: {
             type: DataTypes.STRING,
             allowNull: false,
+            validate: {
+                notNull: {
+                    msg: "El lastName es requerido",
+                },
+                notEmpty: {
+                    msg: "Debe ingresar un lastName",
+                },
+            },
         },
         email: {
             type: DataTypes.STRING,
@@ -25,8 +43,23 @@ const User = sequelize.define(
                 is: /\S+@\S+\.\S+/,
             },
         },
-    },
-    {
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                min: {
+                    args: [8],
+                    msg: "Se requiere un mínimo de 8 caracteres para la contraseña (password)",
+                },
+                notNull: {
+                    msg: "Password es requerido",
+                },
+                notEmpty: {
+                    msg: "Debe ingresar un password",
+                },
+            },
+        },
+    }, {
         timestamps: true,
         tableName: "user",
     }
